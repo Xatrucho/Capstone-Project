9529cf09dbd39700f260f412aeb5f966ec78cf5e
Xatrucho <marlonenriqueponcevillatoro@gmail.com>
2021-11-24 15:54:35 -0600

    installed flask-cors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
